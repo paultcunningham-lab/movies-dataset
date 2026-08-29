a5593b000b4b3cc46cb36c3e03224eefc0a1e4fb
paultcunningham-lab <paultcunningham@gmail.com>
2026-08-29 20:59:20 +0100

    Change Movie Title column width to 20px

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,7 @@ st.dataframe(
     column_config={
         "title": st.column_config.TextColumn(
             "Movie Title",
-            width="200px",  # Options: "small", "medium", "large", or pixel values like "300px"
+            width="20px",  # Options: "small", "medium", "large", or pixel values like "300px"
         )},
     
     use_container_width=True,
